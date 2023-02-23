48edfcf8a06d21af6b9e6b26b943d1322ceb82fe
Jaihanuman64 <mohak64bansal@gmail.com>
2023-02-23 17:24:04 +0530

    #49 jwt auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "mohakisagood$oy";
 
 //Create a user using : POST "/api/auth/". Doesn't require Auth
 router.post(
@@ -23,10 +27,14 @@ router.post(
       if (user) {
         return res.status(400).json({ error: "user with same email exist" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
+        // password: req.body.password,
       });
       //   .then((user) => res.json(user))
       //   .catch((err) => {
@@ -36,7 +44,14 @@ router.post(
       //       message: err.message,
       //     });
       //   });
-      res.json(user);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      // res.json(user);
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("some error occured");

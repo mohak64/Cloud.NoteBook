99d21e479d968d9ec6514e332c2ed4b2f3b3d98a
Jaihanuman64 <mohak64bansal@gmail.com>
2023-02-23 12:55:22 +0530

    everything fine till #46

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");  //npm i -g nodemon
-const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true";
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true";
 mongoose.set("strictQuery", true); //so that no error
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 //Create a user using : POST "/api/auth/". Doesn't require Auth
-router.post("/", (req, res) => {
-  const user = User(req.body);
-  user.save()
-  res.send(req.body);
-});
+router.post(
+  "/",
+  body("name").isLength({ min: 3 }),
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      }).then(user => res.json(user))
+      .catch(err=> {console.log(err)
+    res.json({error: 'please enter a unique value for email', message: err.message})})
+  }
+);
 
 module.exports = router;

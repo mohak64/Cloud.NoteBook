c0fc2fa1bbbda8fddada752d5b402952dc22d6d4
Jaihanuman64 <mohak64bansal@gmail.com>
2023-02-23 10:29:58 +0530

    44 complete

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,14 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Available routes
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

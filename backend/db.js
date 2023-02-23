984c54b3d6c231481252b3d162a8c8606e365b41
Jaihanuman64 <mohak64bansal@gmail.com>
2023-02-23 10:00:37 +0530

    complete 43 thunderclient working

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose");  //npm i -g nodemon
 const mongoURI = "mongodb://localhost:27017/?directConnection=true";
-mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", true); //so that no error
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
-    console.log(" to mongo");
+    console.log("connected to mongo");
   });
 };
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,15 @@
 const connectToMongo = require('./db')
-
+const express = require('express')
 connectToMongo();
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

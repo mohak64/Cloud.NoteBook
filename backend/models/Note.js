343e934c786e6db9f14c4275234832b7a1f412e7
Jaihanuman64 <mohak64bansal@gmail.com>
2023-02-23 20:31:18 +0530

    52 comp

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
-
+const { Schema } = mongoose;
 const NotesSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
   title: {
     type: String,
     required: true,
@@ -14,7 +18,7 @@ const NotesSchema = new Schema({
     default: "General",
   },
   date: {
-    type: date,
+    type: Date,
     default: Date.now,
   },
 });

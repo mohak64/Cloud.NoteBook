8edb21847a787e2afce855440584022fc13fdd64
Jaihanuman64 <mohak64bansal@gmail.com>
2023-02-24 17:10:38 +0530

    56 done!!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
-import './App.css'; 
- 
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNoteBook</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+        <div className="container">
+          <Routes>
+            <Route exact path="/home" element={<Home/>}/>
+              
+            <Route exact path="/about" element={<About/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
